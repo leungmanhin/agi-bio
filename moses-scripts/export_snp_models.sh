f3e86e7132b03b4e9d3a7b3e81ce7ab6b99e484f
leungmanhin <manhinleung@gmail.com>
2020-02-12 17:12:38 +0800

    Add functions for generating the needed implication links

--- moses-scripts/export_snp_models.sh
+++ moses-scripts/export_snp_models.sh
@@ -32,4 +32,127 @@ while getopts "o:" opt; do
     esac
 done
 
+#############
+# Functions #
+#############
+
+# Given
+#
+# 1. a predicate name
+#
+# 2. a combo model
+#
+# 3. a sensitivity value
+#
+# 4. a count
+#
+# return a Scheme code defining the implication between the predicate
+# and the model:
+#
+# ImplicationLink <{3}, {4}>
+#     PredicateNode {1}
+#     {2}
+implication_sensitivity_def() {
+    local pred="$1"
+    local model="$2"
+    local sensitivity="$3"
+    local count="$4"
+    cat <<EOF
+(ImplicationLink (stv $sensitivity, $count)
+    (PredicateNode "$pred")
+    $model)
+EOF
+}
+
+# Given
+#
+# 1. a predicate name
+#
+# 2. a combo model
+#
+# 3. a specificity value
+#
+# 4. a count
+#
+# return a Scheme code defining the implication between the predicate
+# and the model:
+#
+# ImplicationLink <{3}, {4}>
+#     NotLink
+#         PredicateNode {1}
+#     NotLink
+#         {2}
+implication_specificity_def() {
+    local pred="$1"
+    local model="$2"
+    local specificity="$3"
+    local count="$4"
+    cat <<EOF
+(ImplicationLink (stv $specificity, $count)
+    (NotLink
+        (PredicateNode "$pred"))
+    (NotLink
+        $model))
+EOF
+}
+
+# Given
+#
+# 1. a predicate name
+#
+# 2. a combo model
+#
+# 3. a precision value (positive predictive value)
+#
+# 4. a count
+#
+# return a Scheme code defining the implication between the predicate
+# and the model:
+#
+# ImplicationLink <{3}, {4}>
+#     {2}
+#     PredicateNode {1}
+implication_precision_def() {
+    local pred="$1"
+    local model="$2"
+    local precision="$3"
+    local count="$4"
+    cat <<EOF
+(ImplicationLink (stv $precision, $count)
+    $model
+    (PredicateNode "$pred"))
+EOF
+}
+
+# Given
+#
+# 1. a predicate name
+#
+# 2. a combo model
+#
+# 3. a negative predictive value
+#
+# 4. a count
+#
+# return a Scheme code defining the implication between the predicate
+# and the model:
+#
+# ImplicationLink <{3}, {4}>
+#     NotLink
+#         PredicateNode {1}
+#     NotLink
+#         {2}
+implication_neg_pred_val_def() {
+    local pred="$1"
+    local model="$2"
+    local npv="$3"
+    local count="$4"
+    cat <<EOF
+(ImplicationLink (stv $npv, $count)
+    (NotLink
+        $model)
+    (NotLink
+        (PredicateNode "$pred")))
+EOF
+}
 
